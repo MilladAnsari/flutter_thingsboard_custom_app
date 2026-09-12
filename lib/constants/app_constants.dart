712a1c0afa444697e8f49c58917ef30748571934
MilladAnsari <162457552+MilladAnsari@users.noreply.github.com>
2026-09-13 01:21:08 +0330

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,9 +1,7 @@
 import 'package:thingsboard_app/modules/main/model/navigation_type.dart';
 
 abstract final class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = String.fromEnvironment(
-    'thingsboardApiEndpoint',
-  );
+  static const thingsBoardApiEndpoint = 'https://m3hzp96p-18080.euw.devtunnels.ms';
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment(
     'thingsboardOAuth2CallbackUrlScheme',
   );
